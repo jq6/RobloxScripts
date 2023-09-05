22db23aeb29cbd85553cca51683e290e0f470494
jq6 <111148387+jq6@users.noreply.github.com>
2023-09-06 00:16:01 +0300

    Create hdvipkill.lua
    i hate @prohax1

---
+++ hdvipkill.lua
@@ -0,0 +1,50 @@
+--[[
+CODED BY 1_805
+THANKS FOR USE!!!
+AND I HATE SCHOOL!
+
+]]
+
+while wait() do
+local lp=game:GetService("Players").LocalPlayer
+game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+--game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+wait(.2)
+
+
+
+	game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";morph me Chair ")
+    wait(.5)
+    game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";r6 me ")
+    wait(.5)
+local chr=lp.Character
+	repeat wait() until chr:FindFirstChild("Seat")~=nil
+    print("SHOWED SEAT HAT")
+	repeat wait() until chr.Seat.Handle.Seat:FindFirstChild("SeatWeld")~=nil
+    print("SHOWED SEAT WELD")
+	if chr.Seat.Handle.Seat:FindFirstChild("SeatWeld")~=nil then
+    wait(.15)
+		local savdd=chr:FindFirstChild("HumanoidRootPart").CFrame
+        for i=0,20 do
+		chr:WaitForChild("HumanoidRootPart").CFrame=CFrame.new(0,game:GetService("Workspace").FallenPartsDestroyHeight+23,0)
+        end
+        wait(.2)
+		
+
+
+
+game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";morph me Clown ")
+
+
+		repeat wait() until chr:FindFirstChild("Seat")==nil
+chr:WaitForChild("HumanoidRootPart").CFrame=savdd
+wait(.2)
+game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+--game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+		
+        --wait(.15)
+     
+--game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+--game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ")
+	end
+print("END") wait(.5) game:GetService("ReplicatedStorage").HDAdminClient.Signals.RequestCommand:InvokeServer(";refresh me ") wait(.3) end
